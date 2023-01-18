ca684ab16c4b2d4c04aff35a787a567e69fdab11
Sara Iftikhar <sara.rwpk@gmail.com>
2023-01-18 19:22:20 +0900

    added docs folder

---
+++ docs/source/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+from sphinx_gallery.sorting import ExampleTitleSortKey
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'catchment_aware_lstm'
+copyright = '2023, Sara Iftikhar'
+author = 'Sara Iftikhar'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+'sphinx.ext.todo',
+'sphinx.ext.viewcode',
+'sphinx.ext.autodoc',
+'sphinx.ext.autosummary',
+'sphinx.ext.doctest',
+'sphinx.ext.intersphinx',
+'sphinx.ext.imgconverter',
+'sphinx_issues',
+'sphinx.ext.mathjax',
+'sphinx.ext.napoleon',
+'sphinx.ext.githubpages',
+"sphinx-prompt",
+"sphinx_gallery.gen_gallery",
+'sphinx.ext.ifconfig',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['utils.py']
+
+
+sphinx_gallery_conf = {
+    'backreferences_dir': 'gen_modules/backreferences',
+    #'doc_module': ('sphinx_gallery', 'numpy'),
+    'reference_url': {
+        'sphinx_gallery': None,
+    },
+    'examples_dirs': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'scripts'),
+    'gallery_dirs': 'auto_examples',
+    'compress_images': ('images', 'thumbnails'),
+    'filename_pattern': '',
+    'ignore_pattern': 'utils.py',
+
+    #'show_memory': True,
+    #'junit': os.path.join('sphinx-gallery', 'junit-results.xml'),
+    # capture raw HTML or, if not present, __repr__ of last expression in
+    # each code block
+    'capture_repr': ('_repr_html_', '__repr__'),
+    'matplotlib_animations': True,
+    'image_srcset': ["2x"],
+    'within_subsection_order': ExampleTitleSortKey,
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. catchment_aware_lstm documentation master file, created by
+   sphinx-quickstart on Tue Jan 17 23:27:14 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to catchment_aware_lstm's documentation!
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   auto_examples/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
